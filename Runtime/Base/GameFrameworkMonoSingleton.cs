808abe225da80688b3b8963277c154358d4a32fe
AlianBlank <wangfj11@foxmail.com>
2024-05-21 11:24:31 +0800

    feat: 增加Mono单例对象

---
+++ Runtime/Base/GameFrameworkMonoSingleton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Base
+{
+    /// <summary>
+    /// 游戏框架单例
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class GameFrameworkMonoSingleton<T> where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        protected GameFrameworkMonoSingleton()
+        {
+        }
+
+        /// <summary>
+        /// 单例对象
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = (T)Object.FindObjectOfType(typeof(T));
+                }
+
+                if (_instance == null)
+                {
+                    var insObj = new GameObject();
+                    _instance = insObj.AddComponent<T>();
+                    _instance.name = "[Singleton]" + typeof(T);
+
+                    if (Application.isPlaying)
+                    {
+                        Object.DontDestroyOnLoad(insObj);
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
