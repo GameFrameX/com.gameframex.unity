54526e18f0c3227bd5de03dde3625f76eb489ea8
AlianBlank <wangfj11@foxmail.com>
2024-07-30 16:37:11 +0800

    feat: 增加热更新程序集编译定义的适配

---
+++ Editor/BuildHotfix/HotFixEditorCompilerHelper.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+
+namespace GameFrameX.Editor
+{
+    /// <summary>
+    /// 热更新程序集的编辑器编译指令帮助类
+    /// </summary>
+    public static class HotFixEditorCompilerHelper
+    {
+        /// <summary>
+        /// 移除热更新程序集的编辑器编译指令
+        /// </summary>
+        [MenuItem("Tools/Build/HotFix Editor Compiler Remove", false, 100)]
+        public static void RemoveEditor()
+        {
+            var path = "Assets/Hotfix/Unity.HotFix.asmdef";
+            HotFixAssemblyDefinitionHelper.RemoveEditor(path);
+        }
+
+        /// <summary>
+        /// 增加热更新程序集的编辑器编译指令
+        /// </summary>
+        [MenuItem("Tools/Build/HotFix Editor Compiler Add", false, 100)]
+        public static void AddEditor()
+        {
+            var path = "Assets/Hotfix/Unity.HotFix.asmdef";
+            HotFixAssemblyDefinitionHelper.AddEditor(path);
+        }
+    }
+}
