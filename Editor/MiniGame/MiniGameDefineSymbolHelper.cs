886776e147ce83555a89ebcdba8a4636ed49a71f
Blank <wangfj11@foxmail.com>
2024-08-22 11:39:36 +0800

    fix: 修改小游戏宏定义只能在WEBGL下生效

--- Editor/MiniGame/MiniGameDefineSymbolHelper.cs
+++ Editor/MiniGame/MiniGameDefineSymbolHelper.cs
@@ -1,5 +1,7 @@
-﻿using UnityEditor;
+﻿#if UNITY_WEBGL
+using UnityEditor;
 using UnityEngine;
+#endif
 
 namespace GameFrameX.Editor
 {
@@ -14,47 +16,63 @@ namespace GameFrameX.Editor
         /// <summary>
         /// 开启微信小游戏的适配
         /// </summary>
+#if UNITY_WEBGL
         [MenuItem("GameFrameX/MiniGame/Open WeChat MiniGame")]
+#endif
         public static void OpenWeChatMiniGame()
         {
+#if UNITY_WEBGL
             if (!ScriptingDefineSymbols.HasScriptingDefineSymbol(BuildTargetGroup.WebGL, EnableWeChatMiniGameScriptingDefineSymbol))
             {
                 ScriptingDefineSymbols.AddScriptingDefineSymbol(EnableWeChatMiniGameScriptingDefineSymbol);
             }
 
             Debug.Log($"微信小游戏宏定义 [{EnableDouYinMiniGameScriptingDefineSymbol}] 已经打开");
+#endif
         }
 
         /// <summary>
         /// 关闭微信小游戏的适配
         /// </summary>
+#if UNITY_WEBGL
         [MenuItem("GameFrameX/MiniGame/Close WeChat MiniGame")]
+#endif
         public static void CloseWeChatMiniGame()
         {
+#if UNITY_WEBGL
             ScriptingDefineSymbols.RemoveScriptingDefineSymbol(EnableWeChatMiniGameScriptingDefineSymbol);
+#endif
         }
 
         /// <summary>
         /// 开启抖音小游戏的适配
         /// </summary>
+#if UNITY_WEBGL
         [MenuItem("GameFrameX/MiniGame/Open DouYin MiniGame")]
+#endif
         public static void OpenDouYinMiniGame()
         {
+#if UNITY_WEBGL
             if (!ScriptingDefineSymbols.HasScriptingDefineSymbol(BuildTargetGroup.WebGL, EnableDouYinMiniGameScriptingDefineSymbol))
             {
                 ScriptingDefineSymbols.AddScriptingDefineSymbol(EnableDouYinMiniGameScriptingDefineSymbol);
             }
 
             Debug.Log($"抖音小游戏宏定义 [{EnableDouYinMiniGameScriptingDefineSymbol}] 已经打开");
+#endif
         }
 
         /// <summary>
         /// 关闭抖音小游戏的适配
         /// </summary>
+#if UNITY_WEBGL
         [MenuItem("GameFrameX/MiniGame/Close DouYin MiniGame")]
+#endif
         public static void CloseDouYinMiniGame()
         {
+#if UNITY_WEBGL
             ScriptingDefineSymbols.RemoveScriptingDefineSymbol(EnableDouYinMiniGameScriptingDefineSymbol);
+#endif
         }
     }
 }
