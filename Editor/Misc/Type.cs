1478e3904f6e07efd5b10a9234028040390199f1
Blank <wangfj11@foxmail.com>
2024-10-15 15:32:22 +0800

    [删除]1. 删除废弃代码

--- Editor/Misc/Type.cs
+++ Editor/Misc/Type.cs
@@ -5,8 +5,6 @@
 // Feedback: mailto:ellan@gameframework.cn
 //------------------------------------------------------------
 
-using System;
-using GameFrameX;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -22,40 +20,6 @@ namespace GameFrameX.Editor
         private static readonly string[] RuntimeAssemblyNames = Utility.Assembly.GetAssemblies().Where(m => !m.FullName.Contains("Editor")).Select(m => m.FullName).ToArray();
 
         private static readonly string[] RuntimeOrEditorAssemblyNames = Utility.Assembly.GetAssemblies().Select(m => m.FullName).ToArray();
-
-        /// <summary>
-        /// 获取配置路径。
-        /// </summary>
-        /// <typeparam name="T">配置类型。</typeparam>
-        /// <returns>配置路径。</returns>
-        internal static string GetConfigurationPath<T>() where T : ConfigPathAttribute
-        {
-            foreach (var type in Utility.Assembly.GetTypes())
-            {
-                if (!type.IsAbstract || !type.IsSealed)
-                {
-                    continue;
-                }
-
-                foreach (var fieldInfo in type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly))
-                {
-                    if (fieldInfo.FieldType == typeof(string) && fieldInfo.IsDefined(typeof(T), false))
-                    {
-                        return (string)fieldInfo.GetValue(null);
-                    }
-                }
-
-                foreach (var propertyInfo in type.GetProperties(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly))
-                {
-                    if (propertyInfo.PropertyType == typeof(string) && propertyInfo.IsDefined(typeof(T), false))
-                    {
-                        return (string)propertyInfo.GetValue(null, null);
-                    }
-                }
-            }
-
-            return null;
-        }
 
         /// <summary>
         /// 在运行时程序集中获取指定基类的所有子类的名称。
