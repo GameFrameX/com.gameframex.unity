d2480f03bc5097fc483371e7d7ea30dd7da6c116
Blank <wangfj11@foxmail.com>
2024-11-04 20:28:09 +0800

    feat: 增加标记物体为不销毁模式

---
+++ Runtime/Base/ObjectDontDestroyOnLoad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GameFrameX.Runtime
+{
+    /// <summary>
+    /// 标记物体对象为不可销毁
+    /// </summary>
+    public sealed class ObjectDontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+    }
+}
