e73b7bc54446bf5ab71640449de297832f658f9a
Blank <wangfj11@foxmail.com>
2024-11-04 15:50:30 +0800

    feat: 增加绑定属性的扩展功能

---
+++ Runtime/Property/BindableProperty.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace GameFrameX.Runtime
+{
+    public sealed class BindableProperty<T>
+    {
+        private T _value;
+        private Action<T> _onValueChanged;
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        public T Value
+        {
+            get { return _value; }
+            set
+            {
+                if (!Equals(_value, value))
+                {
+                    _value = value;
+                    _onValueChanged?.Invoke(_value);
+                }
+            }
+        }
+
+        private BindableProperty()
+        {
+            _onValueChanged = null;
+        }
+
+        /// <summary>
+        /// 默认构造函数
+        /// </summary>
+        /// <param name="defaultValue">默认值</param>
+        public BindableProperty(T defaultValue = default) : this()
+        {
+            _value = defaultValue;
+        }
+
+
+        /// <summary>
+        /// 注册值变化事件
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public BindableProperty<T> Add(Action<T> callback)
+        {
+            GameFrameworkGuard.NotNull(callback, nameof(callback));
+            _onValueChanged += callback;
+            return this;
+        }
+
+        /// <summary>
+        /// 注册事件
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public BindableProperty<T> RegisterWithInitValue(Action<T> callback)
+        {
+            GameFrameworkGuard.NotNull(callback, nameof(callback));
+            callback?.Invoke(_value);
+            return Add(callback);
+        }
+
+        /// <summary>
+        /// 移除事件
+        /// </summary>
+        /// <param name="callback">事件</param>
+        public void Remove(Action<T> callback)
+        {
+            GameFrameworkGuard.NotNull(callback, nameof(callback));
+            _onValueChanged -= callback;
+        }
+
+        /// <summary>
+        /// 清除事件
+        /// </summary>
+        public void Clear()
+        {
+            _onValueChanged = null;
+        }
+    }
+}
