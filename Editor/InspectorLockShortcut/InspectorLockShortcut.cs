583a091a4370a054fc45b9fac118ce6c7de4d20e
Blank <wangfj11@foxmail.com>
2024-12-07 15:13:55 +0800

    perf: 优化代码结构

--- Editor/InspectorLockShortcut/InspectorLockShortcut.cs
+++ Editor/InspectorLockShortcut/InspectorLockShortcut.cs
@@ -2,17 +2,17 @@
 
 namespace GameFrameX.Editor
 {
-    public class InspectorLockShortcut : EditorWindow
+    public sealed class InspectorLockShortcut : EditorWindow
     {
         [MenuItem("Window/Toggle Inspector Lock %l")]
         private static void ToggleInspectorLock()
         {
             // 获取Inspector窗口
-            var inspectorType   = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+            var inspectorType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.InspectorWindow");
             var inspectorWindow = EditorWindow.GetWindow(inspectorType);
             // 使用反射调用isLocked属性
             var isLockedProperty = inspectorType.GetProperty("isLocked");
-            var isLocked         = (bool)isLockedProperty.GetValue(inspectorWindow);
+            var isLocked = (bool)isLockedProperty.GetValue(inspectorWindow);
             isLockedProperty.SetValue(inspectorWindow, !isLocked);
             // PrefabUtility.CreatePrefab( "Assets/Editor/ToggleInspectorLock.prefab", inspectorWindow);
         }
