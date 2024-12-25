a0a0f9d398ce47b56db9cb8b8458b390b72d87d8
Blank <wangfj11@foxmail.com>
2024-12-25 14:33:01 +0800

    [增加]1. 增加资源文件夹的扩展名和路径常量

--- Runtime/Utility/Utility.Asset.Path.cs
+++ Runtime/Utility/Utility.Asset.Path.cs
@@ -23,6 +23,66 @@
                 public const string BundlesDirectoryName = "Bundles";
 
                 /// <summary>
+                /// 打包资源文件夹Scene名称
+                /// </summary>
+                public const string BundlesDirectorySceneName = "Scene";
+
+                /// <summary>
+                /// 打包资源文件夹Localization名称
+                /// </summary>
+                public const string BundlesDirectoryLocalizationName = "Localization";
+
+                /// <summary>
+                /// 打包资源文件夹Config名称
+                /// </summary>
+                public const string BundlesDirectoryConfigName = "Config";
+
+                /// <summary>
+                /// 打包资源文件夹AOTCode名称
+                /// </summary>
+                public const string BundlesDirectoryAOTCodeName = "AOTCode";
+
+                /// <summary>
+                /// 打包资源文件夹Code名称
+                /// </summary>
+                public const string BundlesDirectoryCodeName = "Code";
+
+                /// <summary>
+                /// 打包资源文件夹Sound名称
+                /// </summary>
+                public const string BundlesDirectorySoundName = "Sound";
+
+                /// <summary>
+                /// 打包资源文件夹Prefab名称
+                /// </summary>
+                public const string BundlesDirectoryPrefabName = "Prefabs";
+
+                /// <summary>
+                /// 打包资源文件夹Video名称
+                /// </summary>
+                public const string BundlesDirectoryVideoName = "Video";
+
+                /// <summary>
+                /// 打包资源文件夹Image名称
+                /// </summary>
+                public const string BundlesDirectoryImageName = "Image";
+
+                /// <summary>
+                /// 打包资源文件夹UI名称
+                /// </summary>
+                public const string BundlesDirectoryUIName = "UI";
+
+                /// <summary>
+                /// 打包资源文件夹Sprite名称
+                /// </summary>
+                public const string BundlesDirectorySpriteName = "Sprite";
+
+                /// <summary>
+                /// 打包资源文件夹Shader名称
+                /// </summary>
+                public const string BundlesDirectoryShaderName = "Shader";
+
+                /// <summary>
                 /// 获取文件路径
                 /// </summary>
                 /// <param name="filePath">相对于Bundles的路径，不要以/开头</param>
@@ -30,6 +90,46 @@
                 public static string GetFilePath(string filePath)
                 {
                     return $"{BundlesPath}/{filePath}";
+                }
+
+                /// <summary>
+                /// 获取图片文件路径
+                /// </summary>
+                /// <param name="filePath">相对于Bundles/Image的路径，不要以/开头,需要携带扩展名</param>
+                /// <returns>返回拼接好的路径</returns>
+                public static string GetImagePath(string filePath)
+                {
+                    return GetCategoryFilePath(BundlesDirectoryImageName, filePath);
+                }
+
+                /// <summary>
+                /// 获取视频文件路径
+                /// </summary>
+                /// <param name="filePath">相对于Bundles/Video的路径，不要以/开头,需要携带扩展名</param>
+                /// <returns>返回拼接好的路径</returns>
+                public static string GetVideoPath(string filePath)
+                {
+                    return GetCategoryFilePath(BundlesDirectoryVideoName, filePath);
+                }
+
+                /// <summary>
+                /// 获取Sprite文件路径
+                /// </summary>
+                /// <param name="filePath">相对于Bundles/Sprite的路径，不要以/开头,需要携带扩展名</param>
+                /// <returns>返回拼接好的路径</returns>
+                public static string GetSpritePath(string filePath)
+                {
+                    return GetCategoryFilePath(BundlesDirectorySpriteName, filePath);
+                }
+
+                /// <summary>
+                /// 获取Sprite文件路径
+                /// </summary>
+                /// <param name="filePath">相对于Bundles/Prefabs的路径，不要以/开头,需要携带扩展名</param>
+                /// <returns>返回拼接好的路径</returns>
+                public static string GetPrefabPath(string filePath)
+                {
+                    return GetCategoryFilePath(BundlesDirectoryPrefabName, filePath);
                 }
 
                 /// <summary>
@@ -46,49 +146,54 @@
                 /// <summary>
                 /// 获取配置文件路径
                 /// </summary>
-                /// <param name="fileName"></param>
-                /// <param name="extension"></param>
-                /// <returns></returns>
+                /// <param name="fileName">相对于Bundles/Config的路径，不要以/开头,需要携带扩展名</param>
+                /// <param name="extension">文件扩展名称</param>
+                /// <returns>返回拼接好的路径</returns>
                 public static string GetConfigPath(string fileName, string extension = ".bytes")
                 {
-                    return $"{BundlesPath}/Config/{fileName}{extension}";
+                    return GetCategoryFilePath(BundlesDirectoryConfigName, $"{fileName}{extension}");
                 }
 
                 /// <summary>
                 /// 获取AOT元数据代码文件路径
                 /// </summary>
-                /// <param name="fileName"></param>
-                /// <param name="extension"></param>
-                /// <returns></returns>
+                /// <param name="fileName">相对于Bundles/AOTCode的路径，不要以/开头,需要携带扩展名</param>
+                /// <param name="extension">文件扩展名称</param>
+                /// <returns>返回拼接好的路径</returns>
                 public static string GetAOTCodePath(string fileName, string extension = ".bytes")
                 {
-                    return $"{BundlesPath}/AOTCode/{fileName}{extension}";
+                    return GetCategoryFilePath(BundlesDirectoryAOTCodeName, $"{fileName}{extension}");
                 }
 
                 /// <summary>
                 /// 获取代码文件路径
                 /// </summary>
-                /// <param name="fileName"></param>
-                /// <param name="extension"></param>
-                /// <returns></returns>
+                /// <param name="fileName">相对于Bundles/Code的路径，不要以/开头,需要携带扩展名</param>
+                /// <param name="extension">文件扩展名称</param>
+                /// <returns>返回拼接好的路径</returns>
                 public static string GetCodePath(string fileName, string extension = ".bytes")
                 {
-                    return $"{BundlesPath}/Code/{fileName}{extension}";
+                    return GetCategoryFilePath(BundlesDirectoryCodeName, $"{fileName}{extension}");
                 }
 
                 /// <summary>
                 /// 获取UI文件路径
                 /// </summary>
                 /// <param name="uiPackageName">UI包名</param>
-                /// <returns></returns>
+                /// <returns>返回拼接好的路径</returns>
                 public static string GetUIPackagePath(string uiPackageName)
                 {
-                    return $"{BundlesPath}/UI/{uiPackageName}/{uiPackageName}";
+                    return GetCategoryFilePath(BundlesDirectoryUIName, $"{uiPackageName}/{uiPackageName}");
                 }
 
+                /// <summary>
+                /// 获取UI文件路径
+                /// </summary>
+                /// <param name="uiPath">UI路径</param>
+                /// <returns>返回拼接好的路径</returns>
                 public static string GetUIPath(string uiPath)
                 {
-                    return $"{BundlesPath}/UI/{uiPath}";
+                    return GetCategoryFilePath(BundlesDirectoryUIName, uiPath);
                 }
 
                 /// <summary>
@@ -96,15 +201,15 @@
                 /// </summary>
                 /// <param name="pathName">路径包含名称</param>
                 /// <param name="extension">扩展名称,默认为.mp3</param>
-                /// <returns></returns>
+                /// <returns>返回拼接好的路径</returns>
                 public static string GetSoundPath(string pathName, string extension = ".mp3")
                 {
                     if (pathName.IndexOf('.') >= 0)
                     {
-                        return $"{BundlesPath}/Sound/{pathName}";
+                        return GetCategoryFilePath(BundlesDirectorySoundName, pathName);
                     }
 
-                    return $"{BundlesPath}/Sound/{pathName}{extension}";
+                    return GetCategoryFilePath(BundlesDirectorySoundName, $"{pathName}{extension}");
                 }
 
                 /// <summary>
@@ -112,31 +217,31 @@
                 /// </summary>
                 /// <param name="pathName">路径包含名称</param>
                 /// <param name="extension">扩展名,默认为.unity</param>
-                /// <returns></returns>
+                /// <returns>返回拼接好的路径</returns>
                 public static string GetScenePath(string pathName, string extension = ".unity")
                 {
                     if (pathName.IndexOf('.') >= 0)
                     {
-                        return $"{BundlesPath}/Scene/{pathName}";
+                        return GetCategoryFilePath(BundlesDirectorySceneName, pathName);
                     }
 
-                    return $"{BundlesPath}/Scene/{pathName}{extension}";
+                    return GetCategoryFilePath(BundlesDirectorySceneName, $"{pathName}{extension}");
                 }
 
                 /// <summary>
                 /// 获取本地化文件路径
                 /// </summary>
                 /// <param name="pathName">路径包含名称</param>
-                /// <param name="extension"></param>
-                /// <returns></returns>
+                /// <param name="extension">文件扩展名</param>
+                /// <returns>返回拼接好的路径</returns>
                 public static string GetLocalizationPath(string pathName, string extension = ".xml")
                 {
                     if (pathName.IndexOf('.') >= 0)
                     {
-                        return $"{BundlesPath}/Localization/{pathName}";
+                        return GetCategoryFilePath(BundlesDirectoryLocalizationName, pathName);
                     }
 
-                    return $"{BundlesPath}/Localization/{pathName}{extension}";
+                    return GetCategoryFilePath(BundlesDirectoryLocalizationName, $"{pathName}{extension}");
                 }
             }
         }
