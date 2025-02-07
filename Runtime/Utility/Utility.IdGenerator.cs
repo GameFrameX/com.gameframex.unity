f7c70c1f536a15cb24a7ed84934ebe7bc1a83c7e
Blank <wangfj11@foxmail.com>
2025-02-07 15:41:16 +0800

    fix: 修改ID生成器的时间起点

--- Runtime/Utility/Utility.IdGenerator.cs
+++ Runtime/Utility/Utility.IdGenerator.cs
@@ -8,11 +8,15 @@ namespace GameFrameX.Runtime
         [UnityEngine.Scripting.Preserve]
         public static class IdGenerator
         {
-            private static readonly DateTime s_UtcTimeStart = new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            /// <summary>
+            /// 全局UTC起始时间，用作计数器的基准时间点
+            /// 设置为2020年1月1日0时0分0秒(UTC)
+            /// </summary>
+            public static readonly DateTime UtcTimeStart = new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
             // 共享计数器
-            private static long _counter = (long)(DateTime.UtcNow - s_UtcTimeStart).TotalSeconds;
-            private static int _intCounter = (int)(DateTime.UtcNow - s_UtcTimeStart).TotalSeconds;
+            private static long _counter = (long)(DateTime.UtcNow - UtcTimeStart).TotalSeconds;
+            private static int _intCounter = (int)(DateTime.UtcNow - UtcTimeStart).TotalSeconds;
 
             /// <summary>
             /// 使用Interlocked.Increment生成唯一ID的方法
