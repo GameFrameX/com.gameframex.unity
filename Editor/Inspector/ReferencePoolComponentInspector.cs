d556381c16bbf54ef93d19fa8ae621178fe2997a
Blank <wangfj11@foxmail.com>
2025-02-07 11:03:11 +0800

    [删除]1. 删除无效的代码引用

--- Editor/Inspector/ReferencePoolComponentInspector.cs
+++ Editor/Inspector/ReferencePoolComponentInspector.cs
@@ -5,7 +5,6 @@
 // Feedback: mailto:ellan@gameframework.cn
 //------------------------------------------------------------
 
-using GameFrameX;
 using System;
 using System.Collections.Generic;
 using System.IO;
