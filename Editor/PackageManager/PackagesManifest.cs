ef88dc6336828ea32129a5693677cdb6c6dbdf1d
Blank <wangfj11@foxmail.com>
2025-09-08 22:16:13 +0800

    chore: 完善 manifest.json 的数据结构

--- Editor/PackageManager/PackagesManifest.cs
+++ Editor/PackageManager/PackagesManifest.cs
@@ -6,10 +6,21 @@ namespace GameFrameX.Editor
     public sealed class PackagesManifest
     {
         [JsonProperty("dependencies")] public Dictionary<string, string> Dependencies = new Dictionary<string, string>();
+        
+        [JsonProperty("scopedRegistries")] public List<ScopedRegistry> ScopedRegistries = new List<ScopedRegistry>();
 
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
         }
+    }
+    
+    public sealed class ScopedRegistry
+    {
+        [JsonProperty("name")] public string Name { get; set; }
+        
+        [JsonProperty("url")] public string Url { get; set; }
+        
+        [JsonProperty("scopes")] public List<string> Scopes = new List<string>();
     }
 }
