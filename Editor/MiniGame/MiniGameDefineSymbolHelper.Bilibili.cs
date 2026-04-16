a911ac4b76a0507a4ae2d5b3a540a08a405e59e2
Blank <wangfj11@foxmail.com>
2026-04-16 11:24:28 +0800

    feat(Editor): 添加Bilibili小游戏适配宏定义支持
    添加 MiniGameDefineSymbolHelper.Bilibili.cs 文件，为Bilibili小游戏平台提供宏定义开关功能。
    包含 EnableBilibiliMiniGame 和 DisableBilibiliMiniGame 两个菜单项，用于在 Unity Editor 中管理 WebGL 平台的编译宏。

---
+++ Editor/MiniGame/MiniGameDefineSymbolHelper.Bilibili.cs
@@ -0,0 +1,66 @@
+#if UNITY_WEBGL
+using UnityEditor;
+using UnityEngine;
+#endif
+
+namespace GameFrameX.Editor
+{
+    public static partial class MiniGameDefineSymbolHelper
+    {
+        /// <summary>
+        /// Bilibili小游戏适配宏定义集合。
+        /// Define symbols for Bilibili mini game adaptation.
+        /// </summary>
+        public static readonly string[] EnableBilibiliMiniGameScriptingDefineSymbol = new[] { "ENABLE_BILIBILI_MINI_GAME", "BILIBILIMINIGAME" };
+
+        /// <summary>
+        /// 开启Bilibili小游戏适配宏定义。
+        /// Enables define symbols for Bilibili mini game adaptation.
+        /// </summary>
+#if UNITY_WEBGL
+        [MenuItem("GameFrameX/Scripting Define Symbols/Enable Bilibili Mini Game(开启[Bilibili小游戏]适配)", false, 2700)]
+#endif
+        public static void EnableBilibiliMiniGame()
+        {
+#if UNITY_WEBGL
+            DisableOtherMiniGameScriptingDefineSymbols(EnableBilibiliMiniGameScriptingDefineSymbol);
+
+            foreach (var define in EnableBilibiliMiniGameScriptingDefineSymbol)
+            {
+                if (!ScriptingDefineSymbols.HasScriptingDefineSymbol(BuildTargetGroup.WebGL, define))
+                {
+                    ScriptingDefineSymbols.AddScriptingDefineSymbol(define);
+                }
+
+                Debug.Log($"Bilibili小游戏宏定义 [{define}] 已经打开");
+            }
+
+            EnableUnifiedMiniGameScriptingDefineSymbol();
+#endif
+        }
+
+        /// <summary>
+        /// 关闭Bilibili小游戏适配宏定义。
+        /// Disables define symbols for Bilibili mini game adaptation.
+        /// </summary>
+#if UNITY_WEBGL
+        [MenuItem("GameFrameX/Scripting Define Symbols/Disable Bilibili Mini Game(关闭[Bilibili小游戏]适配)", false, 2701)]
+#endif
+        public static void DisableBilibiliMiniGame()
+        {
+#if UNITY_WEBGL
+            foreach (var define in EnableBilibiliMiniGameScriptingDefineSymbol)
+            {
+                if (ScriptingDefineSymbols.HasScriptingDefineSymbol(BuildTargetGroup.WebGL, define))
+                {
+                    ScriptingDefineSymbols.RemoveScriptingDefineSymbol(define);
+                }
+
+                Debug.Log($"Bilibili小游戏宏定义 [{define}] 已经关闭");
+            }
+
+            RefreshUnifiedMiniGameScriptingDefineSymbol();
+#endif
+        }
+    }
+}
