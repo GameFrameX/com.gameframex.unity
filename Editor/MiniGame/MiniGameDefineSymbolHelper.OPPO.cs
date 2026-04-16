e73ab828f2f0ca847eee26ba1fe9a6d2bd65f21c
Blank <wangfj11@foxmail.com>
2026-04-16 13:38:52 +0800

    feat: 添加 OPPO 小游戏适配宏定义助手
    新增 MiniGameDefineSymbolHelper.OPPO.cs 文件，提供在 Unity Editor 中启用和禁用 OPPO 小游戏相关编译宏的菜单项，以支持 OPPO 小游戏平台的特定适配。

---
+++ Editor/MiniGame/MiniGameDefineSymbolHelper.OPPO.cs
@@ -0,0 +1,66 @@
+#if UNITY_WEBGL
+using UnityEditor;
+using UnityEngine;
+#endif
+
+namespace GameFrameX.Editor
+{
+    public static partial class MiniGameDefineSymbolHelper
+    {
+        /// <summary>
+        /// OPPO 小游戏适配宏定义集合。
+        /// Define symbols for OPPO mini game adaptation.
+        /// </summary>
+        public static readonly string[] EnableOPPOMiniGameScriptingDefineSymbol = new[] { "ENABLE_OPPO_MINI_GAME", "OPPOSMINIGAME" };
+
+        /// <summary>
+        /// 开启 OPPO 小游戏适配宏定义。
+        /// Enables define symbols for OPPO mini game adaptation.
+        /// </summary>
+#if UNITY_WEBGL
+        [MenuItem("GameFrameX/Scripting Define Symbols/Enable OPPO Mini Game(开启[OPPO小游戏]适配)", false, 3200)]
+#endif
+        public static void EnableOPPOMiniGame()
+        {
+#if UNITY_WEBGL
+            DisableOtherMiniGameScriptingDefineSymbols(EnableOPPOMiniGameScriptingDefineSymbol);
+
+            foreach (var define in EnableOPPOMiniGameScriptingDefineSymbol)
+            {
+                if (!ScriptingDefineSymbols.HasScriptingDefineSymbol(BuildTargetGroup.WebGL, define))
+                {
+                    ScriptingDefineSymbols.AddScriptingDefineSymbol(define);
+                }
+
+                Debug.Log($"OPPO 小游戏宏定义 [{define}] 已经打开");
+            }
+
+            EnableUnifiedMiniGameScriptingDefineSymbol();
+#endif
+        }
+
+        /// <summary>
+        /// 关闭 OPPO 小游戏适配宏定义。
+        /// Disables define symbols for OPPO mini game adaptation.
+        /// </summary>
+#if UNITY_WEBGL
+        [MenuItem("GameFrameX/Scripting Define Symbols/Disable OPPO Mini Game(关闭[OPPO小游戏]适配)", false, 3201)]
+#endif
+        public static void DisableOPPOMiniGame()
+        {
+#if UNITY_WEBGL
+            foreach (var define in EnableOPPOMiniGameScriptingDefineSymbol)
+            {
+                if (ScriptingDefineSymbols.HasScriptingDefineSymbol(BuildTargetGroup.WebGL, define))
+                {
+                    ScriptingDefineSymbols.RemoveScriptingDefineSymbol(define);
+                }
+
+                Debug.Log($"OPPO 小游戏宏定义 [{define}] 已经关闭");
+            }
+
+            RefreshUnifiedMiniGameScriptingDefineSymbol();
+#endif
+        }
+    }
+}
