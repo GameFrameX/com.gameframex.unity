992d4331341e0e639af0bd7ed202b69126e1bb0f
Blank <wangfj11@foxmail.com>
2026-07-16 21:44:33 +0800

    docs(license): 更新文件头授权和仓库地址
    修复 GitHub Issue #11 对应的问题。
    
    Closes #11

--- Runtime/Extension/UnityEngine.Vector4/UnityEngine.Vector4Extension.cs
+++ Runtime/Extension/UnityEngine.Vector4/UnityEngine.Vector4Extension.cs
@@ -7,8 +7,8 @@
 //  使用本项目须严格遵守相应法律法规及开源许可证之规定。
 //  Usage of this project must strictly comply with applicable laws, regulations, and open-source licenses.
 //
-//  本项目采用 MIT 许可证与 Apache License 2.0 双许可证分发，
-//  This project is dual-licensed under the MIT License and Apache License 2.0,
+//  本项目采用 Apache License 2.0 许可证分发，
+//  This project is licensed under the Apache License 2.0,
 //  完整许可证文本请参见源代码根目录下的 LICENSE 文件。
 //  please refer to the LICENSE file in the root directory of the source code for the full license text.
 //
@@ -25,6 +25,8 @@
 //  GitHub Repository: https://github.com/GameFrameX
 //  Gitee  仓库：https://gitee.com/GameFrameX
 //  Gitee Repository:  https://gitee.com/GameFrameX
+//  CNB  仓库：https://cnb.cool/GameFrameX
+//  CNB Repository:  https://cnb.cool/GameFrameX
 //  官方文档：https://gameframex.doc.alianblank.com/
 //  Official Documentation: https://gameframex.doc.alianblank.com/
 // ==========================================================================================
